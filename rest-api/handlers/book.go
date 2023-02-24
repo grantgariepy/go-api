adaa780506c6ad33e39a822c846888d4585e4573
grantgariepy <grantgariepy@protonmail.com>
2023-02-23 19:02:28 -0700

    put

--- rest-api/handlers/book.go
+++ rest-api/handlers/book.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/grantgariepy/rest-api/database"
+	"github.com/grantgariepy/rest-api/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type newBookDTO struct {
@@ -20,6 +24,25 @@ func CreateBook(c *fiber.Ctx) error {
 		return err
 	}
 
-	fmt.Println(createData)
-	return nil
+	// get collection reference
+	coll := database.GetCollection("libraries")
+
+	// get the filter
+	filter := bson.D{{Key: "id", Value: createData.LibraryId}}
+	nBookData := models.Book{
+		Title:  createData.Title,
+		Author: createData.Author,
+		ISBN:   createData.ISBN,
+	}
+	updatePayload := bson.M{"$push": bson.M{"books": nBookData}}
+
+	// update the library
+	res, err := coll.UpdateOne(context.TODO(), filter, updatePayload)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(res.UpsertedID)
+
+	return c.SendString("Book Created Successfully")
 }
